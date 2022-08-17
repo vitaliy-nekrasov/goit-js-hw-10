e3adf6d3221100c7bb48b4017d90ad0710fa8711
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-08-17 16:47:12 +0300

    Add search by country name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -18,21 +19,41 @@ function onCountrySearch(evt) {
     clearMarkup();
     return;
   }
-  fetchCountries(countryName).then(arrayOfCountries => {
-    clearMarkup();
-    arrayOfCountries.forEach(countryObject => {
-      if (arrayOfCountries.length > 1) {
-        addCountriesListMarkup(countryObject);
+  fetchCountries(countryName)
+    .then(arrayOfCountries => {
+      clearMarkup();
+      if (arrayOfCountries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
       }
-    });
-  });
+      arrayOfCountries.forEach(countryObject => {
+        if (arrayOfCountries.length > 1) {
+          addCountriesListMarkup(countryObject);
+        } else {
+          addCountryInfoMarkup(countryObject);
+        }
+      });
+    })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
 }
 
-function addCountriesListMarkup(countryObject) {
-  const string = `<li><img src="${countryObject.flags.svg}" alt=""><p>${countryObject.name.official}</p></li>`;
+function addCountriesListMarkup({ flags, name }) {
+  const string = `<li><img src="${flags.svg}" alt=""><p>${name.official}</p></li>`;
   countryListEl.insertAdjacentHTML('afterbegin', string);
+}
+
+function addCountryInfoMarkup({ flags, name, capital, population, languages }) {
+  const string = `<img src="${flags.svg}" alt="" />
+      <h1>${name.official}</h1>
+      <p>Capital: <span>${capital}</span></p>
+      <p>Population: <span>${population}</span></p>
+      <p>Languages: <span>${Object.values(languages).join(', ')}</span></p>`;
+  countyInfoEl.insertAdjacentHTML('afterbegin', string);
 }
 
 function clearMarkup() {
   countryListEl.innerHTML = '';
+  countyInfoEl.innerHTML = '';
 }
